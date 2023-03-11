67a9353dd1e297777de93e032495591ea0492f62
Jorge Ibarra <hello@jorgeibarra.com.mx>
2023-03-11 17:15:52 -0600

    Added some get methods and took out the semicolons

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Query } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
@@ -7,6 +7,42 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo método'
+  }
+
+  // @Get('products')
+  // getProducts(@Query() params: any) {
+  //   const { limit, offset } = params
+  //   return `products ${limit}, ${offset}`
+  // }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100, //infers the type
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products ${limit}, ${offset}, ${brand}`
+  }
+
+  //Always use static routes first and then dynamic routes.
+  @Get('products/filter')
+  getProductFilter() {
+    return `yo soy un filter`
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product ${productId} and category ${id}`
   }
 }
