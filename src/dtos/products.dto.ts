f471f2c60666041e20b983b905c223c19e626fa9
Jorge Ibarra <hello@jorgeibarra.com.mx>
2023-03-14 21:55:37 -0600

    Added class-validator and mapped-types dependencies, used them in products DTO

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,15 +1,39 @@
+import { PartialType } from '@nestjs/mapped-types'
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+} from 'class-validator'
+
 export class CreateProductDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly stock: number
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string
 }
 
-export class UpdateProductDTO {
-  readonly name?: string
-  readonly description?: string
-  readonly price?: number
-  readonly stock?: number
-  readonly image?: string
+export class UpdateProductDTO extends PartialType(CreateProductDTO) {
+  // readonly name?: string
+  // readonly description?: string
+  // readonly price?: number
+  // readonly stock?: number
+  // readonly image?: string
 }
